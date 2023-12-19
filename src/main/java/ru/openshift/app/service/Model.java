5fcc0302438f5c32b1449cf42af8cb985c48504a
PavelArtenov <myemail@mail.com>
2023-12-19 16:52:02 +0300

    tmp

---
+++ src/main/java/ru/openshift/app/service/Model.java
@@ -0,0 +1,18 @@
+package ru.openshift.app.service;
+
+public class Model {
+
+    private Long id;
+
+    public Model(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
